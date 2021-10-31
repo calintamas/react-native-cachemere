187db9df602c25f9fbd52b41e1146dfec369df6b
Calin Tamas <calintamas2@gmail.com>
2021-10-31 09:38:39 +0200

    feat: generic Cache and Storage classes

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,6 @@ module.exports = {
   extends: ['backpacker-react-ts'],
   rules: {
     'import/no-extraneous-dependencies': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/lines-between-class-members': 'off',
-    'arrow-body-style': 'off',
-    'no-underscore-dangle': 'off',
-  },
-}
+    '@typescript-eslint/explicit-module-boundary-types': 'off'
+  }
+};
--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,6 @@
-export const TTL_12H = 43200
-export const TTL_8H = 28800
-export const TTL_6H = 21600
-export const TTL_4H = 14400
-export const TTL_1H = 3600
+export const TTL_30M = 1800;
+export const TTL_1H = TTL_30M * 2;
+export const TTL_4H = TTL_1H * 4;
+export const TTL_6H = TTL_1H * 6;
+export const TTL_8H = TTL_1H * 8;
+export const TTL_12H = TTL_1H * 12;
