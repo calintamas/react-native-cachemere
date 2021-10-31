b7a03b4c0189d35b56b7ce554227a0bdd09c541b
Alexandru Mic <alexandru.mic07@gmail.com>
2021-10-31 21:55:14 +0200

    feat: handle FIFO replacement policy

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,3 +1,7 @@
 export function addPrefix(str: string, prefix: string) {
   return `${prefix}${str}`;
 }
+
+export function stripPrefix(str: string, prefix: string) {
+  return str.replace(prefix, '');
+}
